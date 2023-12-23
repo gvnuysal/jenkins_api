54967300f937083e50d0db4bf896886b081cd653
guven.uysal <gvnuysal@gmail.com>
2023-12-23 20:16:51 +0300

    .

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Jenkins.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Güven"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Güven","Gamze","Deniz Kemal"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
