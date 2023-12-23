89e87e3d034c73c6615d0e6ee683968610e67cea
guven.uysal <gvnuysal@gmail.com>
2023-12-23 12:55:14 +0300

    .

--- obj/Debug/net5.0/Jenkins.AssemblyInfo.cs
+++ obj/Debug/net5.0/Jenkins.AssemblyInfo.cs
@@ -11,6 +11,7 @@
 using System;
 using System.Reflection;
 
+[assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("d87ee094-6dbf-4030-96a5-d6653edaed74")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("Jenkins")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
