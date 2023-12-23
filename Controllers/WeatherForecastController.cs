acf27947ff3c564512a018b1fb516605590b9b05
guven.uysal <gvnuysal@gmail.com>
2023-12-23 20:12:51 +0300

    added push

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Jenkins.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching","Güven"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
